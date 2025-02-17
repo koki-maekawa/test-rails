b8563c522fab1b61cbf4807f6c63cc075061ae4a
koki-maekawa <koki.study33@gmail.com>
2025-02-17 20:31:33 +0900

    chore: test222

--- spec/system/top_pages_spec.rb
+++ spec/system/top_pages_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe 'Static Pages', :js, type: :system do
     it 'トップページ表示' do
         visit root_path
 
-        expect(page).to have_selector('li', text: 'Sign Up')
+        expect(page).to have_selector('li', text: 'xign Up')
     end
 end
